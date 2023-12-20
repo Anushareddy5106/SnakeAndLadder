adaef053340df7274f7853b6bd063d899c26b8dd
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-20 15:47:34 +0530

    project initialization

---
+++ src/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to the Snake and Ladder game");
+
+        int playerPos = 0;
+
+    }
+}
