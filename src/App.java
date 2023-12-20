4abd86fdbc6b091089e91459782428fea1db439f
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-20 16:12:16 +0530

    Position of player and number of times dice was played  included

--- src/App.java
+++ src/App.java
@@ -24,31 +24,40 @@ public class App {
 
         int playerPos = 0;
 
+        // UC6
+        int numDiceRoll = 0;
+
         // UC4 and UC5
         while (playerPos < 100) {
             int diceRoll = diceRoll();
             int playOption = playOption();
 
+            System.out.println("----->Move " + numDiceRoll + ":  ");
+            System.out.println("diceroll: " + diceRoll + " ");
+
             switch (playOption) {
                 case 0:
-                    System.out.println("Snake");
+                    System.out.println("PlayOption: Snake");
                     playerPos = (playerPos - diceRoll) < 0 ? 0 : playerPos - diceRoll;
                     break;
                 case 1:
-                    System.out.println("Ladder");
+                    System.out.println("PlayOption: Ladder");
                     playerPos = (playerPos + diceRoll) > 100 ? playerPos : playerPos + diceRoll;
                     break;
 
                 default:
-                    System.out.println("No play");
+                    System.out.println("PlayOption: No play");
                     break;
             }
 
             System.out.println("Position of the Player:" + playerPos);
+            numDiceRoll++;
 
             if (playerPos == 100)
                 break;
         }
+
+        System.out.println("Number of times the dice was played: " + numDiceRoll);
 
     }
 }
