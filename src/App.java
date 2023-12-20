980f842419fc92ecb528a37cd83ac33cecfd53fb
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-20 15:55:17 +0530

    Dice Rolling

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
+import java.util.Random;
 
 public class App {
 
+    // UC2
+    public static int diceRoll() {
+        Random random = new Random();
+        int number = random.nextInt(6) + 1;
+
+        return number;
+    }
+
+    // UC1
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the Snake and Ladder game");
 
