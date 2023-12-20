d3bde8ae36e2073ec334681f59f569ee81e192c0
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-20 16:03:18 +0530

    Repeating until playerPos is 100

--- src/App.java
+++ src/App.java
@@ -24,5 +24,31 @@ public class App {
 
         int playerPos = 0;
 
+        // UC4 and UC5
+        while (playerPos < 100) {
+            int diceRoll = diceRoll();
+            int playOption = playOption();
+
+            switch (playOption) {
+                case 0:
+                    System.out.println("Snake");
+                    playerPos = (playerPos - diceRoll) < 0 ? 0 : playerPos - diceRoll;
+                    break;
+                case 1:
+                    System.out.println("Ladder");
+                    playerPos = (playerPos + diceRoll) > 100 ? playerPos : playerPos + diceRoll;
+                    break;
+
+                default:
+                    System.out.println("No play");
+                    break;
+            }
+
+            System.out.println("Position of the Player:" + playerPos);
+
+            if (playerPos == 100)
+                break;
+        }
+
     }
 }
